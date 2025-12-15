c38e649bed39f57b22f225606e75f4fd1fa54bb3
23-101125-RAIA <23-101125@students.eui.edu.eg>
2025-12-15 07:27:37 +0200

    section2

--- js.js
+++ js.js
@@ -10,6 +10,24 @@
   document.getElementById("nav-link4").innerHTML = "About"
   document.getElementById("section1-title").innerHTML = "Dragon Motors — Where legacy meets innovation since"
 
+document.getElementById("title-sec2").innerHTML = "AI-Powered Driving";
+document.getElementById("text-sec2").innerHTML ="Step into the future with an AI-infused machine built to understand you, protect you, and elevate every journey";
+document.getElementById("cta-sec2").innerHTML = "Pre-Order Now";
+document.getElementById("subtext-sec2").innerHTML = "Electric Powertrain";
+document.getElementById("stat-number").innerHTML = "65%";
+document.getElementById("subtext-sec2-1").innerHTML = "Smart Autopilot";
+document.getElementById("stat-number1").innerHTML = "80%";
+document.getElementById("subtext-sec2-2").innerHTML = "Location";
+document.getElementById("stat-number2").innerHTML = "74%";
+document.getElementById("subtitle-sec2").innerHTML = "5G";
+document.getElementById("subtext-sec2-3").innerHTML = "Smart Connectivity";
+document.getElementById("subtitle-sec2-1").innerHTML = "80k";
+document.getElementById("subtext-sec2-4").innerHTML = "Fast-Charging System";
+document.getElementById("subtitle-sec2-2").innerHTML = "360°";
+document.getElementById("subtext-sec2-5").innerHTML = "Surround Sensor Vision";
+
+
+
 
 
 
