f1c008f08c8003dee4d2ed37fc530755ddf10696
23-101125-RAIA <23-101125@students.eui.edu.eg>
2025-12-15 04:02:09 +0200

    logo

--- js.js
+++ js.js
@@ -3,5 +3,10 @@
       document.getElementById("preloader").style.display = "none";
     }, 4000);
   });
+
+  document.getElementById("nav-link1").innerHTML = "Home"
+  document.getElementById("nav-link2").innerHTML = "Products"
+  document.getElementById("nav-link3").innerHTML = "Event"
+  document.getElementById("nav-link4").innerHTML = "About"
     
   
